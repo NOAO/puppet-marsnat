174777e902ce5a2b2ab22ab04a3ddfeb0e3fc29e
Steve Pothier <pothiers@gmail.com>
2020-03-09 14:52:11 -0700

    hardwire devops uid

--- manifests/install.pp
+++ manifests/install.pp
@@ -59,6 +59,7 @@ class marsnat::install (
     # tadapassword/xxxx_||.x.
     password   => '$1$Pk1b6yel$tPE2h9vxYE248CoGKfhR41',  
     system     => true,
+    uid        => 661,
     groups     => ['tada','cache'],
   } ->
 
