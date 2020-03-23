532d2cf34177830caa4092b3a8ff255bb0686d92
Peter Peterson <peter.peterson.iv@gmail.com>
2020-03-23 09:39:34 -0700

    setting up logging

--- files/elasticsearch_logging/download.sh
+++ files/elasticsearch_logging/download.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 
 # filebeats
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.1.0-linux-x86_64.tar.gz
-tar xzvf filebeat-7.1.0-linux-x86_64.tar.gz
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.1.0-x86_64.rpm
+rpm -vi filebeat-7.1.0-x86_64.rpm
 
 # metricbeats
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.1.0-linux-x86_64.tar.gz
-tar xzvf metricbeat-7.1.0-linux-x86_64.tar.gz
+curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.1.0-x86_64.rpm
+rpm -vi metricbeat-7.1.0-x86_64.rpm
 
--- files/elasticsearch_logging/setup.sh
+++ files/elasticsearch_logging/setup.sh
@@ -4,23 +4,16 @@
 export elastic_url=$@
 export replace='{{ elastic_url }}'
 
-rm /opt/filebeat-*/filebeat.yml
-rm /opt/metricbeat-*/metricbeat.yml
 
 # copy the config into place
 # replace the string with url
-sed -e "s/$replace/$elastic_url/ig" filebeat.yml > /opt/filebeat-7.1.0-linux-x86_64/filebeat.yml
-sed -e "s/$replace/$elastic_url/ig" metricbeat.yml > /opt/metricbeat-7.1.0-linux-x86_64/metricbeat.yml
+sed -e "s/$replace/$elastic_url/ig" filebeat.yml > /etc/filebeat/filebeat.yml
+sed -e "s/$replace/$elastic_url/ig" metricbeat.yml > /etc/filebeat/metricbeat.yml
 
-cd /opt/filebeat-7.1.0-linux-x86_64
-# set ownership of files
-chown root:root -R filebeat.yml modules.d module
-/opt/filebeat modules enable nginx
-/opt/filebeat modules enable system
-/opt/filebeat setup
+filebeat modules enable nginx
+filebeat modules enable system
+filebeat setup
 
-cd /opt/metricbeat-7.1.0-linux-x86_64
-chown root:root -R metricbeat.yml modules.d module
-/opt/metricbeat modules enable nginx
-/opt/metricbeat setup
+metricbeat modules enable nginx
+metricbeat setup
 
